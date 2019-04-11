8367ad6c7345467ca2c2cabe7042f7d0ebf48663
dbroms <49290240+dbroms@users.noreply.github.com>
2019-04-10 22:05:03 -0700

    Update location_tests.py

--- location_tests.py
+++ location_tests.py
@@ -3,11 +3,18 @@ from location import *
 
 class TestLab1(unittest.TestCase):
 
-    def test_repr(self):
+    def test_repr1(self):
         loc = Location("SLO", 35.3, -120.7)
         self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
-    
-    # Add more tests!
+    def test_repr2(self):
+        loc = Location("LJ", -41.4, 37.9)
+        self.assertEqual(repr(loc),"Location('LJ', -41.4, 37.9)")
+    def test_repr3(self):
+        loc = Location("LA", 35.8, 84.4)
+        self.assertEqual(repr(loc),"Location('LA', 35.8, 84.4)")
+    def test_repr4(self):
+        loc = Location("Bend", -31.7, -91.5)
+        self.assertEqual(repr(loc),"Location('Bend', -31.7, -91.5)")
 
 if __name__ == "__main__":
         unittest.main()
