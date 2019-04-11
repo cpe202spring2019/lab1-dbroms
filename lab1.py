0251ca60ddef5689f694380b9a64f1ea9e10c099
dbroms <49290240+dbroms@users.noreply.github.com>
2019-04-10 22:01:27 -0700

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,9 +1,8 @@
-
 def max_list_iter(int_list):  # must use iteration not recursion
     """finds the max of a list of numbers and returns the value (not the index)
     If int_list is empty, returns None. If list is None, raises ValueError"""
     if int_list == None:
-      raise ValueError
+        raise ValueError
     elif int_list == []:
         return None
     else:
@@ -14,22 +13,28 @@ def max_list_iter(int_list):  # must use iteration not recursion
         return max
 
 def reverse_rec(int_list):   # must use recursion
-   """recursively reverses a list of numbers and returns the reversed list
-   If list is None, raises ValueError"""
+    """recursively reverses a list of numbers and returns the reversed list
+    If list is None, raises ValueError"""
     if int_list == None:
-       raise ValueError
+        raise ValueError
     elif int_list == []:
-       return None
+        return int_list
     return int_list[-1:] + reverse_rec(int_list[:-1])
 
 
 def bin_search(target, low, high, int_list):  # must use recursion
-   """searches for target in int_list[low..high] and returns index if found
-   If target is not found returns None. If list is None, raises ValueError """
+    """searches for target in int_list[low..high] and returns index if found
+    If target is not found returns None. If list is None, raises ValueError """
     if int_list == None:
-       raise ValueError
+        raise ValueError
     elif int_list == []:
-       return None
-    if int_list[0] == target:
-        return 0
-    return bin_search(target, low, high, int_list[1:])
+        return None
+    elif high < low :
+        return None
+    mid= (low + high)//2
+    if target == int_list[mid]:
+        return mid
+    elif target < int_list[mid]:
+        return bin_search(target, low, mid-1, int_list)
+    else:
+        return bin_search(target, mid+1, high, int_list)
